2eda71978044508104f6aaa83add3567bceb381e
git <3257252603@qq.com>
2023-05-10 00:16:26 +0800

    first-page

--- src/main/java/com/ischoolbar/programmer/util/Contents.java
+++
@@ -1,9 +0,0 @@
-package com.ischoolbar.programmer.util;
-
-public class Contents {
-    public static final String url = "jdbc:mysql://localhost:3306/student" +
-            "?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=false&allowPublicKeyRetrieval=true";//???????
-    public static final String username = "root";//??????????
-    public static final String password = "hcy080325...";//?????????
-    public static final String driver = "com.mysql.cj.jdbc.Driver";//mysql????
-}
